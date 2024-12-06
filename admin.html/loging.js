5f50f9e9956e47e891062dcee588e575dd7d172c
unknown <ruvindusharada22@gmail.com>
2024-12-06 22:46:46 +0530

    first commit

---
+++ admin.html/loging.js
@@ -0,0 +1,10 @@
+
+function loging(){
+ let username=document.getElementById("Username").value;
+ let password=document.getElementById("password").value;
+ if(username=='Ruvindu'&& password==95087443){
+    window.location.href='admin.html' 
+ }else{
+     alert("Wrong passWord")
+ }
+}
